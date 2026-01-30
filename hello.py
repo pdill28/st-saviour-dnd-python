91c049ac9139c4f24b00d0dcd3541a0b60a373f9
pdillon <pdillon28@stsaviour.org>
2026-01-30 11:45:44 -0500

    lab 4 done!

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     
     song = Music('Us', 'Gracie Abrams', 'The Secret of Us', 3.2)
     print(song.listen())
- 
+  #this is the first instance
     another_song = Music('Blinding lights', 'The Weekend', 'After Hours', 3.5)
     print(another_song.listen())
+   #this is the second instance
